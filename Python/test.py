6caae20c8217156245d310ae272f8c9c4820ae82
Roman Rozhalovskiy <roroman0303@gmail.com>
2021-11-12 20:44:31 +0300

    reminder

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,5 @@
 my_dict = {'a': 3, 'c': 1, 'b': 2, '0': 3}
-new_dict = sorted(my_dict.items(), key= lambda item: item[0])
+keys = [1, 2, 3, 4]
+values = [5, 6, 7, 8]
+new_dict = my_dict.fromkeys(keys, values)
 print(new_dict)
